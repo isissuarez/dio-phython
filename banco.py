65036503c967db5dc628c6679c5cce5e345d2332
isissuarez <122058927+isissuarez@users.noreply.github.com>
2023-07-13 19:37:35 -0300

    Update banco.py

--- banco.py
+++ banco.py
@@ -1,19 +1,26 @@
 
 
-saldo = 0
+saldo = 500
 limite_saques_valor = 500
-extrato = []
+extrato = list(
+
+
+)
 numero_saques = 0
 limite_saques = 3
 
+extrato.append(saldo)
+print(extrato)
 
 def deposito():
     global saldo
+    global  extrato
     valor = int(input("digíte o valor que deseja depositar"))
     if valor > 0:
         saldo = saldo + valor
         print("deposito realizado com sucesso")
-        print(f"{saldo}")
+        extrato.append(valor)
+        extrato.append(saldo)
         
     else:
         print("valor inválido")
@@ -21,17 +28,25 @@ def deposito():
 def saque():
     global saldo
     global limite_saques
+    global  extrato
     valor = int(input("digite o valor que deseja sacar\n"))
     if(saldo > 0 and valor < 500):
         if(limite_saques > 0):
             limite_saques = limite_saques - 1
             saldo = saldo - valor
             print("saque efetuado com sucesso! \n")
-            print(f"{saldo}")
+            extrato.append(saldo)
+            extrato.append(valor)
            
     elif(saldo <= 0):
         print("Você não possui saldo em conta para efetuar esta operação")
         
     elif(valor > 500):
         print("seu limite de saque diário é de 500,00\n")
+
+def mostra_extrato():
+
+     print(f'{extrato}')
+
+
         
